1028183e41418b066e65f6f5c0ce79ac09f99700
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-11-10 22:14:59 +0900

    Feature : 즐겨찾는 웹페이지 불러오기 구현
    - WKWebView 클래스를 익스텐션으로 확장해서 load(favoriteWebPage:) 메서드를 추가했다
    - load(favoriteWebPage:)를 ViewController의 viewDidLoad()에서 호출한다

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -5,14 +5,36 @@
 // 
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        webView.load(favoriteWebPage: .google)
     }
-
-
 }
 
+extension WKWebView {
+    /// 즐겨찾는 웹페이지
+    enum FavoriteWebPage: String {
+        case google = "https://www.google.com/"
+        case yagomdDotNet = "https://yagom.net/"
+        case naver = "https://www.naver.com/"
+    }
+    
+    /// 즐겨찾는 웹페이지 불러오기
+    func load(favoriteWebPage: FavoriteWebPage) {
+        guard let url = URL(string: favoriteWebPage.rawValue) else {
+            fatalError("URL 변환 실패")
+        }
+        
+        let request: URLRequest = URLRequest(url: url)
+        
+        load(request)
+    }
+}
