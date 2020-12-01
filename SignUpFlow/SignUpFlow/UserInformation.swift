1f0a0e52d827c9fa96eb0d7e2e87079fe213286f
sole <subuchappyv2@gmail.com>
2020-12-01 12:51:50 +0900

    Feat: 싱글톤으로 UserInformation클래스 생성

--- SignUpFlow/SignUpFlow/UserInformation.swift
+++ SignUpFlow/SignUpFlow/UserInformation.swift
@@ -1,8 +1,17 @@
-//
-//  UserInformation.swift
-//  SignUpFlow
-//
-//  Created by sole on 2020/12/01.
-//
 
 import Foundation
+import UIKit
+
+class UserInformation {
+    static let common: UserInformation = UserInformation()
+    
+    private init() {}
+    
+    private(set) var userList = [String : User]()
+    
+    func addNewUser(id: String, user: User) {
+        userList[id] = user
+    }
+}
+
+
