39fe21bb1e3e2f7ad2c5012bcec62249a70b38df
lina0322 <llim@student.42seoul.kr>
2020-12-02 17:38:45 +0900

    Feat : detail page 이동버튼 구현

--- SignUpFlow/SignUpFlow/SignUpDetailViewController.swift
+++ SignUpFlow/SignUpFlow/SignUpDetailViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class SignUpDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBAction func dismissSignUpView() {
+        dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func popToPrev() {
+           self.navigationController?.popViewController(animated: true)
+       }
+    
+    
+    @IBAction func completeSignUp() {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
 }
