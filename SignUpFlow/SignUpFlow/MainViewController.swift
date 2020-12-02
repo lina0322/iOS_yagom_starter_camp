e524f050349aad0f5e399e65d1931ed810229e7f
sole <subuchappyv2@gmail.com>
2020-12-02 11:39:43 +0900

    Feat : 로그인 알럿구현

--- SignUpFlow/SignUpFlow/MainViewController.swift
+++ SignUpFlow/SignUpFlow/MainViewController.swift
@@ -27,8 +27,31 @@ class MainViewController: UIViewController, UITextFieldDelegate {
             passwordTextField.becomeFirstResponder()
         } else {
             passwordTextField.resignFirstResponder()
+            systemAlert()
         }
         return true
+    }
+    
+    @IBAction func touchUpSignIn(_ sender: UIButton) {
+        systemAlert()
+    }
+    
+    func systemAlert() {
+        if idTextField.text == "" {
+            showAlert(about: "아이디를 입력해주세요.")
+        } else if passwordTextField.text == "" {
+            showAlert(about: "비밀번호를 입력해주세요.")
+        } else {
+            showAlert(about: "로그인 기능이 비활성화 되어있습니다.")
+        }
+    }
+    
+    func showAlert(about message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(okButton)
+        present(alert, animated: true, completion: nil)
     }
 }
 
