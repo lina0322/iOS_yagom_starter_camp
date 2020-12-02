8be44df48738d8903162bad94b0b91b9c13f75ab
lina0322 <llim@student.42seoul.kr>
2020-12-02 13:04:26 +0900

    Refator : 아이디 입력, 비밀번호 입력과 관련한 알림창 제거

--- SignUpFlow/SignUpFlow/MainViewController.swift
+++ SignUpFlow/SignUpFlow/MainViewController.swift
@@ -27,27 +27,17 @@ class MainViewController: UIViewController, UITextFieldDelegate {
             passwordTextField.becomeFirstResponder()
         } else {
             passwordTextField.resignFirstResponder()
-            systemAlert()
+            touchUpSignIn(nil)
         }
         return true
     }
     
-    @IBAction func touchUpSignIn(_ sender: UIButton) {
-        systemAlert()
+    @IBAction func touchUpSignIn(_ sender: UIButton?) {
+        showCanNotSignInAlert()
     }
     
-    func systemAlert() {
-        if idTextField.text == "" {
-            showAlert(about: "아이디를 입력해주세요.")
-        } else if passwordTextField.text == "" {
-            showAlert(about: "비밀번호를 입력해주세요.")
-        } else {
-            showAlert(about: "로그인 기능이 비활성화 되어있습니다.")
-        }
-    }
-    
-    func showAlert(about message: String) {
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+    func showCanNotSignInAlert() {
+        let alert = UIAlertController(title: nil, message: "로그인 할 수 없습니다.", preferredStyle: .alert)
         let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(okButton)
