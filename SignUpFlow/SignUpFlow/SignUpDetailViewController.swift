261ceab5eb57e9382e31aa4ce0b307d8238e44c7
lina0322 <llim@student.42seoul.kr>
2020-12-03 20:52:59 +0900

    Feat : checkCanFinish

--- SignUpFlow/SignUpFlow/SignUpDetailViewController.swift
+++ SignUpFlow/SignUpFlow/SignUpDetailViewController.swift
@@ -9,21 +9,27 @@ import UIKit
 
 class SignUpDetailViewController: UIViewController {
     
-    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var doneButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        phoneNumberTextField.delegate = self
+        
         datePicker.timeZone = NSTimeZone.local
         datePicker.date = Date()
         datePicker.maximumDate = Date()
-        changeDateLabel(to: datePicker.date)
         datePicker.addTarget(self, action: #selector(onDidChangeDate(_:)), for: .valueChanged)
+        
+        doneButton.isEnabled = false
     }
     
     @objc func onDidChangeDate(_ sender: UIDatePicker) {
         let date = sender.date
+        
         changeDateLabel(to: date)
     }
     
@@ -32,6 +38,18 @@ class SignUpDetailViewController: UIViewController {
         
         format.dateFormat = "MMMM d, yyyy"
         dateLabel.text = format.string(from: date)
+        checkCanFinish()
+    }
+    
+    func checkCanFinish() {
+        guard let phoneNumber = phoneNumberTextField.text,
+              !phoneNumber.isEmpty,
+              let dateOfbirth = dateLabel.text,
+              !dateOfbirth.isEmpty else {
+            doneButton.isEnabled = false
+            return
+        }
+        doneButton.isEnabled = true
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -49,5 +67,17 @@ class SignUpDetailViewController: UIViewController {
     
     @IBAction func completeSignUp() {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SignUpDetailViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        checkCanFinish()
     }
 }
