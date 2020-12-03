3773f27f047b2734b1a52839980b27fff7ffe1f4
lina0322 <llim@student.42seoul.kr>
2020-12-03 20:25:26 +0900

    Feat : datePicker 구현

--- SignUpFlow/SignUpFlow/SignUpDetailViewController.swift
+++ SignUpFlow/SignUpFlow/SignUpDetailViewController.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class SignUpDetailViewController: UIViewController {
     
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        datePicker.maximumDate = Date()
+        datePicker.timeZone = NSTimeZone.local
+        datePicker.addTarget(self, action: #selector(onDidChangeDate(_:)), for: .valueChanged)
+    }
+    
+    @objc func onDidChangeDate(_ sender: UIDatePicker) {
+        let date = sender.date
+        let format = DateFormatter()
+        
+        format.dateFormat = "MMMM d, yyyy"
+        dateLabel.text = format.string(from: date)
+    }
+    
+    @objc func pickDate() {
+        
+    }
+    
     @IBAction func dismissSignUpView() {
         dismiss(animated: true, completion: nil)
     }
