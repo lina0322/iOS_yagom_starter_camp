1ae9861fe6e8186674e04ee9029b129472eafc30
lina0322 <llim@student.42seoul.kr>
2020-12-04 13:25:08 +0900

    Refactor : touchUpSignIn

--- SignUpFlow/SignUpFlow/MainViewController.swift
+++ SignUpFlow/SignUpFlow/MainViewController.swift
@@ -28,23 +28,36 @@ class MainViewController: UIViewController, UITextFieldDelegate {
             passwordTextField.becomeFirstResponder()
         } else {
             passwordTextField.resignFirstResponder()
-            touchUpSignIn(nil)
+            signIn()
         }
         return false
     }
     
     @IBAction func touchUpSignIn(_ sender: UIButton?) {
+        signIn()
+    }
+    
+    func signIn() {
+        guard isAllFull() else {
+            return
+        }
+        // 아이디와 패스워드를 확인하고 맞으면 로그인하는 힘수 들어갈 자리
+        // 만약 없는 아이디나 잘못된 패스워드이면 아래 문구 출력
+        changeStatusMessage(to: .disableSignIn)
+    }
+    
+    func isAllFull() -> Bool {
         guard idTextField.isFilled() else {
             changeStatusMessage(to: .enterId)
-            return
+            return false
         }
         
         guard passwordTextField.isFilled() else {
             changeStatusMessage(to: .enterPassword)
-            return
+            return false
         }
         
-        changeStatusMessage(to: .disableSignIn)
+        return true
     }
     
     private func changeStatusMessage(to message: Message) {
--- SignUpFlow/SignUpFlow/message.swift
+++ SignUpFlow/SignUpFlow/message.swift
@@ -11,6 +11,6 @@ enum Message: String {
     case existedId = "이미 존재하는 아이디입니다."
     case enterId = "아이디를 입력해주세요."
     case enterPassword = "패스워드를 입력해주세요."
-    case disableSignIn = "지금은 로그인 할 수 없습니다."
+    case disableSignIn = "로그인에 실패하였습니다."
     case empty = ""
 }
