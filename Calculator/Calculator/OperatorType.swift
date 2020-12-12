69983ce5eb2cf9e1890e370779fa2b964eacf575
taetae <taetae.dev@gmail.com>
2020-12-12 16:00:57 +0900

    Add: 연산자 타입 정의

--- Calculator/Calculator/OperatorType.swift
+++ Calculator/Calculator/OperatorType.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+enum OperatorType: String, CaseIterable {
+    case add = "+"
+    case subtract = "-"
+    case multiple = "*"
+    case divide = "/"
+    case and = "&"
+    case nand = "~&"
+    case or = "|"
+    case nor = "~|"
+    case xor = "^"
+    case not = "~"
+    case leftShift = "<<"
+    case rightShift = ">>"
+    
+
+}
