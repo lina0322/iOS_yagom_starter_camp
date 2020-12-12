237561666ad2afea6bd3a0a982e556feb9682261
taetae <taetae.dev@gmail.com>
2020-12-12 16:26:47 +0900

    feat: 이진연산 기능 추가 및 피연산자 타입변경
    10진 계산기는 Double형 이지만 2진 계산기는 Int형
    따라서 override를 통해 해당 부분 수정 및 2진 계산 연산기능 추가

--- Calculator/Calculator/BinaryCalculator.swift
+++ Calculator/Calculator/BinaryCalculator.swift
@@ -15,4 +15,33 @@ class BinaryCalculator: Calculator {
             super.determineOperatorType(operator: `operator`)
         }
     }
+    
+    override func operate(calculatorOperator: OperatorType, firstValue: String, secondValue: String = Constants.zero) -> String {
+        guard let firstNumber = Int(firstValue, radix: 2),
+              let secondNumber = Int(secondValue, radix: 2) else { return Constants.zero }
+        switch calculatorOperator {
+        case .add:
+            return String(firstNumber + secondNumber, radix: 2)
+        case .subtract:
+            return String(firstNumber - secondNumber, radix: 2)
+        case .and:
+            return String(firstNumber & secondNumber, radix: 2)
+        case .nand:
+            return String(~(firstNumber & secondNumber), radix: 2)
+        case .or:
+            return String(firstNumber | secondNumber, radix: 2)
+        case .nor:
+            return String(~(firstNumber | secondNumber), radix: 2)
+        case .xor:
+            return String(firstNumber ^ secondNumber, radix: 2)
+        case .not:
+            return String(~firstNumber, radix: 2)
+        case .leftShift:
+            return String(firstNumber << 1, radix: 2)
+        case .rightShift:
+            return String(firstNumber >> 1, radix: 2)
+        default:
+            return Constants.zero
+        }
+    }
 }
