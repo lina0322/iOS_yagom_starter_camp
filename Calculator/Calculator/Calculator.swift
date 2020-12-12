429962bd8759e76f1c3cdc74fc37d2d21d306c5e
taetae <taetae.dev@gmail.com>
2020-12-12 16:09:52 +0900

    feat: 계산기 모드에 따른 초기화

--- Calculator/Calculator/Calculator.swift
+++ Calculator/Calculator/Calculator.swift
@@ -1,8 +1,23 @@
-//
-//  Calculator.swift
-//  Calculator
-//
-//  Created by 김태형 on 2020/12/12.
-//
 
 import Foundation
+
+class Calculator {
+    
+    var currentMode: CalculatorMode
+    var postfix: [String] = []
+    var stack: Stack = Stack()
+    var resultValue = Constants.zero
+    var operators: [String] = OperatorType.allCases.map {
+        type -> String in return type.rawValue
+    }
+    
+    init(calculatorMode: CalculatorMode) {
+        currentMode = calculatorMode
+        operators = operators.filter ({ (value: String) -> Bool in
+            if let `operator` = OperatorType(rawValue: value) {
+                return `operator`.mode == CalculatorMode.common || `operator`.mode == currentMode
+            }
+            return false
+        })
+    }
+}
