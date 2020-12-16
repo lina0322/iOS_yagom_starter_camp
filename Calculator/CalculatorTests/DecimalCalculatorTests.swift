805c9409b3cede7bfcc5e1459d2e1ce998b9481a
taetae <taetae.dev@gmail.com>
2020-12-16 18:14:09 +0900

    Add: 10진 계산기 테스트 코드 proto

--- Calculator/CalculatorTests/DecimalCalculatorTests.swift
+++ Calculator/CalculatorTests/DecimalCalculatorTests.swift
@@ -1,32 +1,106 @@
 //
-//  DecimalCalculator.swift
+//  DecimalCalculatorTests.swift
 //  CalculatorTests
 //
-//  Created by 김태형 on 2020/12/16.
+//  Created by 김태형 on 2020/12/15.
 //
 
 import XCTest
+@testable import Calculator
 
-class DecimalCalculator: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+final class DecimalCalculatorTests: XCTestCase {
+    private var sut: DecimalCalculator!
+    
+    override func setUp() {
+        super.setUp()
+        sut = DecimalCalculator()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testAdd() {
+        XCTAssertEqual(sut.operate(calculatorOperator: .add, firstValue: "1", secondValue: "2"), "3.0")
+        XCTAssertEqual(sut.operate(calculatorOperator: .add, firstValue: "3", secondValue: "-2"), "1.0")
+        XCTAssertEqual(sut.operate(calculatorOperator: .add, firstValue: "-1", secondValue: "2"), "1.0")
+        XCTAssertEqual(sut.operate(calculatorOperator: .add, firstValue: "-2", secondValue: "-3"), "-5.0")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSubtract() {
+        XCTAssertEqual(sut.operate(calculatorOperator: .subtract, firstValue: "2", secondValue: "1"), "1.0")
+        XCTAssertEqual(sut.operate(calculatorOperator: .subtract, firstValue: "1", secondValue: "-2"), "3.0")
+        XCTAssertEqual(sut.operate(calculatorOperator: .subtract, firstValue: "-1", secondValue: "3"), "-4.0")
+        XCTAssertEqual(sut.operate(calculatorOperator: .subtract, firstValue: "-1", secondValue: "-3"), "2.0")
     }
+    
+    func testMultiple() {
+        XCTAssertEqual(sut.operate(calculatorOperator: .multiple, firstValue: "3", secondValue: "4"), "12.0")
+    }
+    
+    func testDivide() {
+        XCTAssertEqual(sut.operate(calculatorOperator: .divide, firstValue: "10", secondValue: "2"), "5.0")
+    }
+    
+//  0으로 나누었을 때 에러 코드 추가 필요
+//    func testDivideByzero() {
+//        XCTAssertEqual(sut.operate(calculatorOperator: .divide, firstValue: "15", secondValue: "0"), "error")
+//    }
+    
+    func testIsOperator() {
+        XCTAssertEqual(sut.isOperator("+"), true)
+        XCTAssertEqual(sut.isOperator("-"), true)
+        XCTAssertEqual(sut.isOperator("*"), true)
+        XCTAssertEqual(sut.isOperator("/"), true)
+        XCTAssertEqual(sut.isOperator("="), false)
+        XCTAssertEqual(sut.isOperator(")"), false)
+    }
+    
+    func testIsEqual() {
+        XCTAssertEqual(sut.isEqual("="), true)
+        XCTAssertEqual(sut.isEqual("*"), false)
+    }
+    
+    func testHandleDigit() {
+        //정수 + 소수 = 9자리 이상일때 정수부터 총 9자리까지만 출력
+        sut.handleInput("1234.123456")
+        sut.handleInput("+")
+        sut.handleInput("1111.111111")
+        sut.handleInput("=")
+        XCTAssertEqual(sut.resultValue, "2345.23456")
+        
+        //정수 9자리 이상일때 1의자리 부터 총 9자리만 출력
+        sut.handleInput("111122223333")
+        sut.handleInput("+")
+        sut.handleInput("222233334444")
+        sut.handleInput("=")
+        XCTAssertEqual(sut.resultValue, "355557777")
+   }
 
+    func testCalculateVariousIntValues() {
+        sut.handleInput("10")
+        sut.handleInput("+")
+        sut.handleInput("2")
+        sut.handleInput("*")
+        sut.handleInput("3")
+        sut.handleInput("-")
+        sut.handleInput("4")
+        sut.handleInput("/")
+        sut.handleInput("2")
+        sut.handleInput("=")
+        
+        XCTAssertEqual(sut.resultValue, "14")
+        
+    }
+    
+    func testCalculateVariousDoubleValues() {
+        sut.handleInput("13.234")
+        sut.handleInput("*")
+        sut.handleInput("21.3132")
+        sut.handleInput("=")
+        
+        XCTAssertEqual(sut.resultValue, "282.058888") // 9자리 초과 반올림적용 필요
+        
+    }
 }
