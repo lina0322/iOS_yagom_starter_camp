23054d3936c87876bdc399ebf590401de0b5afc8
lina0322 <llim@student.42seoul.kr>
2021-01-19 01:37:22 +0900

    Feat: toast message 띄우기

---
+++ WeatherForecast/WeatherForecast/StringFormattingError.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherError.swift
+//  WeatherForecast
+//
+//  Created by 리나 on 2021/01/19.
+//
+
+enum StringFormattingError: Error, CustomStringConvertible {
+    case notAllowedLocationService
+    case unknown
+
+    var description: String {
+        switch self {
+        case .notAllowedLocationService:
+            return "위치 서비스를 승인하지 않으면,\n 기본 위치로 날씨를 조회합니다."
+        case .unknown:
+            return "알 수 없는 에러가 발생했습니다."
+        }
+    }
+}
--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -106,6 +106,31 @@ extension ViewController: CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        showToast(message: StringFormattingError.notAllowedLocationService.description)
         setUpData()
+    }
+}
+
+extension UIViewController {
+    func showToast(message : String) {
+        let labelWidth: CGFloat = 250
+        let labelHeight: CGFloat = 35
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - labelWidth/2, y: self.view.frame.size.height-100, width: labelWidth, height: labelHeight))
+        
+        toastLabel.backgroundColor = .gray
+        toastLabel.textColor = .white
+        toastLabel.font = .systemFont(ofSize: 14.0)
+        toastLabel.textAlignment = .center
+        toastLabel.text = message
+        toastLabel.layer.cornerRadius = 10
+        toastLabel.clipsToBounds = true
+        toastLabel.numberOfLines = 0
+        
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 10.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
     }
 }
