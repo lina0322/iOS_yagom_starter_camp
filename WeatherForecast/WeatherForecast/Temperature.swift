956af1e5bc78e6b5b13346468cde87eed6640eec
lina0322 <llim@student.42seoul.kr>
2021-01-18 20:38:33 +0900

    Feat: Temperature

--- WeatherForecast/WeatherForecast/Temperature.swift
+++ WeatherForecast/WeatherForecast/Temperature.swift
@@ -5,4 +5,14 @@
 //  Created by 리나 on 2021/01/18.
 //
 
-import Foundation
+struct Temperature: Codable {
+    let average: Double
+    let min: Double
+    let max: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case average = "temp"
+        case min = "temp_min"
+        case max = "temp_max"
+    }
+}
