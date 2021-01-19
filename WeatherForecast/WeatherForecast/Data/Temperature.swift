0cadc9fcd7e8fd4b7c0bf076ea2131cf2852746d
lina0322 <llim@student.42seoul.kr>
2021-01-19 11:18:12 +0900

    Refactor: changeToCelsiusText를 Temperature 내부로 이동

--- WeatherForecast/WeatherForecast/Data/Temperature.swift
+++ WeatherForecast/WeatherForecast/Data/Temperature.swift
@@ -5,14 +5,33 @@
 //  Created by 리나 on 2021/01/18.
 //
 
+import Foundation
+
 struct Temperature: Decodable {
-    let average: Double
-    let min: Double
-    let max: Double
+    let kelvinAverage: Double
+    let kelvinMinimum: Double
+    let kelvinMaximum: Double
+    
+    var celsiusAverage: String {
+        return changeToCelsiusText(kelvinAverage)
+    }
+    var celsiusMinimum: String {
+        return changeToCelsiusText(kelvinMinimum)
+    }
+    var celsiusMaximum: String {
+        return changeToCelsiusText(kelvinMaximum)
+    }
 
     enum CodingKeys: String, CodingKey {
-        case average = "temp"
-        case min = "temp_min"
-        case max = "temp_max"
+        case kelvinAverage = "temp"
+        case kelvinMinimum = "temp_min"
+        case kelvinMaximum = "temp_max"
+    }
+    
+    private func changeToCelsiusText(_ temperature: Double) -> String {
+        let celsius = UnitTemperature.celsius.converter.value(fromBaseUnitValue: temperature)
+        let celsiusText = String(format: InitialValue.celsiusFormat, celsius)
+        
+        return celsiusText
     }
 }
