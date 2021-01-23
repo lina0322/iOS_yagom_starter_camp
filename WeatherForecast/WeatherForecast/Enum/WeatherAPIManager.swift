a66cc1b259376c97c34eac09db1bb12f774b5d1d
lina0322 <llim@student.42seoul.kr>
2021-01-23 16:00:51 +0900

    Refactor: APIManager의 url 생성 방식 및 이름 수정

--- WeatherForecast/WeatherForecast/Enum/WeatherAPIManager.swift
+++ WeatherForecast/WeatherForecast/Enum/WeatherAPIManager.swift
@@ -7,40 +7,37 @@
 
 import Foundation
 
-class WeatherAPIManager {
-    enum Kind {
+struct WeatherAPIManager {
+    enum APIType: CustomStringConvertible {
         case currentWeather
         case forecast
         
-        var dataURL: String {
-            let url = "https://api.openweathermap.org/data/2.5"
+        var description: String {
             switch self {
             case .currentWeather:
-                return "\(url)/weather?"
+                return "weather?"
             case .forecast:
-                return "\(url)/forecast?"
+                return "forecast?"
             }
         }
     }
     
-    static let shared = WeatherAPIManager()
     static let imageURL = "https://openweathermap.org/img/w/%@.png"
-
-    private init () {}
+    private static let baseURL = "https://api.openweathermap.org/data/2.5"
     
-    func makeURLRequest(kind: Kind, latitude: Double, logitude: Double) -> URLRequest? {
-        guard var urlComponents = URLComponents(string: kind.dataURL) else {
+    static func makeURLRequest(apiType: APIType, latitude: Double, logitude: Double) -> URLRequest? {
+        guard var absoluteURL = URLComponents(string: "\(baseURL) + \(apiType)") else {
             return nil
         }
-        urlComponents.queryItems = makeQueryItems(latitude: latitude, logitude: logitude)
-        guard let url = urlComponents.url else {
+        
+        absoluteURL.queryItems = makeQueryItems(latitude: latitude, logitude: logitude)
+        guard let url = absoluteURL.url else {
             return nil
         }
-        let urlRequest = URLRequest(url: url)
-        return urlRequest
+        return URLRequest(url: url)
     }
     
-    private func makeQueryItems(latitude: Double, logitude: Double) -> [URLQueryItem] {
+    private static func makeQueryItems(latitude: Double, logitude: Double) -> [URLQueryItem] {
         let myKey = "4119f1d1ea30af76104279475caf11c7"
         let units = "metric" // 섭씨
         
