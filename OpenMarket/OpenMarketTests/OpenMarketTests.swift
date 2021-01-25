2cda1dd6182ab16a1fcb304d1b63c603c10962c6
lina0322 <llim@student.42seoul.kr>
2021-01-26 01:24:21 +0900

    Feat: Product.swift

---
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -0,0 +1,32 @@
+//
+//  OpenMarketTests.swift
+//  OpenMarketTests
+//
+//  Created by 임리나 on 2021/01/26.
+//
+
+import XCTest
+
+class OpenMarketTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
