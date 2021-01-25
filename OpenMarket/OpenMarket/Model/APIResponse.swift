2f44311c956e2a251255024bee78ceca8cb5b9fa
zziro95 <opers7878@gmail.com>
2021-01-26 04:34:41 +0900

    Feat: API로 부터 데이터를 가져오고 디코딩 기능 구현

---
+++ OpenMarket/OpenMarket/Model/APIResponse.swift
@@ -0,0 +1,8 @@
+//
+//  APIResponse.swift
+//  OpenMarket
+//
+//  Created by Jinho Choi on 2021/01/26.
+//
+
+import Foundation
---
+++ OpenMarket/OpenMarket/Model/ProductsAPIManager.swift
@@ -0,0 +1,8 @@
+//
+//  ProductsAPIManager.swift
+//  OpenMarket
+//
+//  Created by Jinho Choi on 2021/01/26.
+//
+
+import Foundation
