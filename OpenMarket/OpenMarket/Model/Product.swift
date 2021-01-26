cd0874bb30c1b9e8c234bd98ce8584ef73c29304
lina0322 <llim@student.42seoul.kr>
2021-01-27 00:40:07 +0900

    Refactor: Product 타입 인코딩도 할 수 있게 변경

--- OpenMarket/OpenMarket/Model/Product.swift
+++ OpenMarket/OpenMarket/Model/Product.swift
@@ -7,25 +7,59 @@
 
 import Foundation
 
-struct Product: Decodable {
-    let id: Int
-    let title: String
+struct Product: Codable {
+    let id: Int?
+    let title: String?
     let descriptions: String?
-    let price: Int
-    let currency: String
-    let stock: Int
+    let price: Int?
+    let currency: String?
+    let stock: Int?
     let discountedPrice: Int?
-    let thumbnails: [String]
+    let thumbnails: [String]?
     let images: [String]?
-    private let timeStampDate: Double
+    let timeStampDate: Double?
+    let password: String?
     
-    var registrationDate: Date {
-        return Date(timeIntervalSince1970: timeStampDate)
+    init(forPost password: String, title: String, descriptions: String, price: Int, currency: String, stock: Int, discountedPrice: Int? = nil, images: [String]) {
+        self.password = password
+        self.title = title
+        self.descriptions = descriptions
+        self.price = price
+        self.currency = currency
+        self.stock = stock
+        self.discountedPrice = discountedPrice
+        self.images = images
+        
+        self.id = nil
+        self.thumbnails = nil
+        self.timeStampDate = nil
+    }
+    
+    init(forPatch password: String, title: String? = nil, descriptions: String? = nil, price: Int? = nil, currency: String? = nil, stock: Int? = nil, discountedPrice: Int? = nil, images: [String]? = nil) {
+        self.password = password
+        
+        self.id = nil
+        self.title = title
+        self.descriptions = descriptions
+        self.price = price
+        self.currency = currency
+        self.stock = stock
+        self.discountedPrice = discountedPrice
+        self.thumbnails = nil
+        self.images = images
+        self.timeStampDate = nil
     }
     
     enum CodingKeys: String, CodingKey {
-        case id, title, descriptions, price, currency, stock, thumbnails, images
+        case id, title, descriptions, price, currency, stock, thumbnails, images, password
         case discountedPrice = "discounted_price"
         case timeStampDate = "registration_date"
+    }
+    
+    func makeRegistrationDate() -> Date? {
+        guard let timeStampDate = timeStampDate else {
+            return nil
+        }
+        return Date(timeIntervalSince1970: timeStampDate)
     }
 }
