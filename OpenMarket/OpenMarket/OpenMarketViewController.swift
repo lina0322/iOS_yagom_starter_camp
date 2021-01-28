84b368d734eaf7449004c17260e550bd9bf697af
zziro95 <opers7878@gmail.com>
2021-01-29 00:54:52 +0900

    Fix: ViewController -> OpenMarketViewController 네이밍 수정

--- OpenMarket/OpenMarket/OpenMarketViewController.swift
+++ OpenMarket/OpenMarket/OpenMarketViewController.swift
@@ -6,13 +6,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OpenMarketViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
