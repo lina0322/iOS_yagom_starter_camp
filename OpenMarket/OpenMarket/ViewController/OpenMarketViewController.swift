75b7c92ac8ba5560d34d46461a705c59d0f054af
zziro95 <opers7878@gmail.com>
2021-01-29 01:08:19 +0900

    Feat: setUpView

--- OpenMarket/OpenMarket/ViewController/OpenMarketViewController.swift
+++ OpenMarket/OpenMarket/ViewController/OpenMarketViewController.swift
@@ -7,10 +7,24 @@
 import UIKit
 
 class OpenMarketViewController: UIViewController {
-
+    let listViewController = ListViewController()
+    let gridViewController = GRIDViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpView()
+    }
+    
+    private func setUpView() {
+        addChild(listViewController)
+        addChild(gridViewController)
         
+        self.view.addSubview(listViewController.view)
+        self.view.addSubview(gridViewController.view)
+        
+        listViewController.view.frame = self.view.bounds
+        gridViewController.view.frame = self.view.bounds
+        gridViewController.view.isHidden = true
     }
 }
 
