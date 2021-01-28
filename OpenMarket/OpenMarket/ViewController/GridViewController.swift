744ad54392c4cf463e6038c99fa066200df4c2c3
zziro95 <opers7878@gmail.com>
2021-01-29 00:59:34 +0900

    Feat: ListViewController, GridViewController 파일 추가

---
+++ OpenMarket/OpenMarket/ViewController/GridViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GridViewController.swift
+//  OpenMarket
+//
+//  Created by Jinho Choi on 2021/01/29.
+//
+
+import UIKit
+
+class GridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ OpenMarket/OpenMarket/ViewController/ListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ListViewController.swift
+//  OpenMarket
+//
+//  Created by Jinho Choi on 2021/01/29.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
