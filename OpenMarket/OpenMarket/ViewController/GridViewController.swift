6ce81bac04c22b2f0ea23f761f4675e928d51d8a
zziro95 <opers7878@gmail.com>
2021-01-30 23:33:20 +0900

    Feat: CollectionView 기본 틀 구현

--- OpenMarket/OpenMarket/ViewController/GridViewController.swift
+++ OpenMarket/OpenMarket/ViewController/GridViewController.swift
@@ -8,28 +8,55 @@
 import UIKit
 
 final class GridViewController: UIViewController {
-//    let collectionView = UICollectionView()
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        configureTableView()
+        configureCollectionView()
     }
-//
-//    private func configureTableView() {
-//        configureConstraintToSafeArea(for: collectionView)
-//        collectionView.dataSource = self
-//        self.collectionView.register(ProductCollectionViewCell.self, forCellWithReuseIdentifier: ProductCollectionViewCell.identifier)
-//    }
+
+    private func configureCollectionView() {
+        configureConstraintToSafeArea(for: collectionView)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(ProductCollectionViewCell.self, forCellWithReuseIdentifier: ProductCollectionViewCell.identifier)
+        
+        collectionView.backgroundColor = .darkGray
+    }
 }
-//
-//extension GridViewController: UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        <#code#>
-//    }
-//
-//
-//}
+
+// MARK: - CollectionView Delegate FlowLayout
+extension GridViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSpacing: CGFloat = 10
+        let width: CGFloat = (collectionView.frame.width - itemSpacing) / 2
+        let height: CGFloat = collectionView.frame.height / 2.5
+        return CGSize(width: width, height: height)
+    }
+    
+}
+
+// MARK: - CollectionView DataSource
+extension GridViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductCollectionViewCell.identifier, for: indexPath) as? ProductCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.backgroundColor = .black
+        return cell
+    }
+}
+
+// MARK: - CollectionView Delegate
+extension GridViewController: UICollectionViewDelegate {
+    
+}
