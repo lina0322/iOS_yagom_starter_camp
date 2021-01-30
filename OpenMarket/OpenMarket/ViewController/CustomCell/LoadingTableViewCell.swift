dadfe78674f83f673310791e499fd32aed0e694f
lina0322 <llim@student.42seoul.kr>
2021-01-30 16:56:19 +0900

    Refactor: loadingTableViewCell

--- OpenMarket/OpenMarket/ViewController/CustomCell/LoadingTableViewCell.swift
+++ OpenMarket/OpenMarket/ViewController/CustomCell/LoadingTableViewCell.swift
@@ -8,16 +8,45 @@
 import UIKit
 
 class LoadingTableViewCell: UITableViewCell {
+    static var identifier: String {
+        return "\(self)"
+    }
+    let activityIndicatorView: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        return indicator
+    }()
+    let label: UILabel = {
+       let label = UILabel()
+        label.text = "마지막 페이지입니다:)"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontSizeToFitWidth = true
+        label.font = .preferredFont(forTextStyle: .body)
+        return label
+    }()
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func start() {
+        addSubview(activityIndicatorView)
+        
+        let safeArea = self.safeAreaLayoutGuide
+        activityIndicatorView.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: safeArea.centerYAnchor).isActive = true
+        activityIndicatorView.startAnimating()
     }
-
+    
+    func stop() {
+        addSubview(label)
+        
+        let safeArea = self.safeAreaLayoutGuide
+        label.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: safeArea.centerYAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
