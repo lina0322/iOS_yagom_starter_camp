a3735df23c425b1cc0c4d08d998dc8f6ac8523a1
lina0322 <llim@student.42seoul.kr>
2021-01-30 12:51:43 +0900

    Feat: 메인 스토리보드에 Launch를 위한 view 추가

---
+++ OpenMarket/OpenMarket/Model/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  OpenMarket
+//
+//  Created by 임리나 on 2021/01/30.
+//
+
+import Foundation
---
+++ OpenMarket/OpenMarket/ViewController/LaunchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LaunchViewController.swift
+//  OpenMarket
+//
+//  Created by 임리나 on 2021/01/30.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
