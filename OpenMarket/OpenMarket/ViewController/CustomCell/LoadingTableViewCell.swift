a4ea659028926c00342289ab3b5ac682eacb8312
lina0322 <llim@student.42seoul.kr>
2021-01-30 16:50:31 +0900

    Refactor: OpenMarketData의 ProductList 타입 변경

---
+++ OpenMarket/OpenMarket/ViewController/CustomCell/LoadingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LoadingTableViewCell.swift
+//  OpenMarket
+//
+//  Created by 임리나 on 2021/01/30.
+//
+
+import UIKit
+
+class LoadingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
