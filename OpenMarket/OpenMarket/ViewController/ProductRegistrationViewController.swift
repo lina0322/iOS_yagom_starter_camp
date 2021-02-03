24a70b0d4d021c1aa55e259701be852429ff6526
lina0322 <llim@student.42seoul.kr>
2021-02-04 02:39:46 +0900

    Feat: 키보드때문에 화면이 가려지는 것 해결

--- OpenMarket/OpenMarket/ViewController/ProductRegistrationViewController.swift
+++ OpenMarket/OpenMarket/ViewController/ProductRegistrationViewController.swift
@@ -3,7 +3,7 @@
 //  OpenMarket
 //
 //  Created by Jinho Choi on 2021/01/30.
-// TODO! password란에 secure설정! 지금 키체인 때문에 꺼놓음
+// TODO! password란에 secure설정! 지금 키체인 때문에 꺼놓음, 키보드때문에 가려지는것
 
 import UIKit
 
@@ -11,14 +11,36 @@ final class ProductRegistrationViewController: UIViewController {
     @IBOutlet private var textFields: [UITextField]!
     @IBOutlet private var descriptionView: UITextView!
     @IBOutlet private var imageCountLabel: UILabel!
+    @IBOutlet private var scrollView: UIScrollView!
     private let cancelButton = UIButton()
     var navigationTitle = String.empty
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        registerNotification()
         configureNavigatinbar()
         configureKeyboardDoneButton()
+    }
+    
+    func registerNotification(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+       }
+       
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+            let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+                return
+        }
+        let contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardFrame.size.height + 150, right: 0.0)
+        scrollView.contentInset = contentInset
+        scrollView.scrollIndicatorInsets = contentInset
+    }
+    
+    @objc private func keyboardWillHide() {
+        let contentInset = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+        scrollView.scrollIndicatorInsets = contentInset
     }
     
     @IBAction func touchUpAddImageButton() {
@@ -33,9 +55,9 @@ final class ProductRegistrationViewController: UIViewController {
         
         toolBarKeyboard.items = [flexibleSpace, doneButton]
         descriptionView.inputAccessoryView = toolBarKeyboard
-        textFields[2].inputAccessoryView = toolBarKeyboard
-        textFields[3].inputAccessoryView = toolBarKeyboard
-        textFields[4].inputAccessoryView = toolBarKeyboard
+        for textField in textFields {
+            textField.inputAccessoryView = toolBarKeyboard
+        }
     }
     
     @objc func touchUpDoneButton(_ sender: UIButton) {
