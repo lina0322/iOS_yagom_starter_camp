edaa3e242d3d6eec13c6d0a41e62770fc6f568f2
lina0322 <llim@student.42seoul.kr>
2021-02-05 10:53:16 +0900

    Add: DetailViewController.swift, 스토리보드에 해당 뷰 추가

---
+++ OpenMarket/OpenMarket/ViewController/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  OpenMarket
+//
+//  Created by 임리나 on 2021/02/05.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
