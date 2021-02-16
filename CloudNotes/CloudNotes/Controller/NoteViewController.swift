bf27bb0ef44dcf13865a83a1b5cb2fc8060f4b95
lina0322 <llim@student.42seoul.kr>
2021-02-16 23:40:15 +0900

    Feat: NoteViewController에서 디코딩하고, 테이블뷰 데이터 소스로 내용 채움

--- CloudNotes/CloudNotes/Controller/NoteViewController.swift
+++ CloudNotes/CloudNotes/Controller/NoteViewController.swift
@@ -7,9 +7,42 @@
 import UIKit
 
 final class NoteViewController: UIViewController {
+    private let tableView = UITableView()
+    private var notes: [Note] = []
+    private let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        let locale = Locale.autoupdatingCurrent.identifier
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale = Locale(identifier: locale)
+        return dateFormatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let data = NSDataAsset(name: NoteString.sample)?.data else {
+            debugPrint(NoteError.wrongData.localizedDescription)
+            return
+        }
+        
+        decodeData(data)
+        setTableView()
         configureNavigationBar()
+    }
+    
+    func decodeData(_ data: Data) {
+        guard let noteData = NoteJSONDecoder.decodeData(data) else {
+            debugPrint(NoteError.wrongData.localizedDescription)
+            return
+        }
+        notes.append(contentsOf: noteData)
+    }
+    
+    func setTableView() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.dataSource = self
+        tableView.register(NoteTableViewCell.self, forCellReuseIdentifier: NoteTableViewCell.identifier)
     }
     
     func configureNavigationBar() {
@@ -20,5 +53,24 @@ final class NoteViewController: UIViewController {
     
     @objc func addNote() {
         
+    }
+}
+
+extension NoteViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NoteTableViewCell.identifier, for: indexPath) as? NoteTableViewCell else {
+            debugPrint("hi")
+            return UITableViewCell()
+        }
+        let note = notes[indexPath.row]
+        cell.titleLabel.text = note.title
+        cell.dateLabel.text = dateFormatter.string(from: note.lastModifiedDate)
+        cell.detailLabel.text = note.body
+        cell.accessoryType = .disclosureIndicator
+        return cell
     }
 }
