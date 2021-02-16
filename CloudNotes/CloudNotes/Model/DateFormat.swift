d68d4bf444c71049025f35d605b61216739fe12b
taetae <taetae.dev@gmail.com>
2021-02-16 22:53:24 +0900

    refactor: 지역 변경에 따라 Date Format이 변경되도록 수정

--- CloudNotes/CloudNotes/Model/DateFormat.swift
+++ CloudNotes/CloudNotes/Model/DateFormat.swift
@@ -9,13 +9,13 @@ import Foundation
 
 struct DateFormat {
     func convertFormat(unixTimeStamp: Int) -> String {
-        let date = Date(timeIntervalSince1970: TimeInterval(unixTimeStamp))
         let dateFormatter = DateFormatter()
-        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
-        dateFormatter.locale = NSLocale.current
-        dateFormatter.dateFormat = "yyyy. MM. dd." 
+        let date = Date(timeIntervalSince1970: TimeInterval(unixTimeStamp))
+        let locale = Locale.current.identifier
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale = Locale(identifier: locale)
         let strDate = dateFormatter.string(from: date)
-        
         return strDate
     }
 }
