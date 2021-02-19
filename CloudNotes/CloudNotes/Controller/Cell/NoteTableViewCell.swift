b2d003075121d37cb552f2d1043bfddd586e02de
taetae <taetae.dev@gmail.com>
2021-02-19 19:47:27 +0900

    refactor: NoteTableViewCell 정리

--- CloudNotes/CloudNotes/Controller/Cell/NoteTableViewCell.swift
+++ CloudNotes/CloudNotes/Controller/Cell/NoteTableViewCell.swift
@@ -8,32 +8,29 @@
 import UIKit
 
 class NoteTableViewCell: UITableViewCell {
-    static let identifier: String = "NoteTableViewCell"
+    static var identifier: String {
+        return "\(self)"
+    }
     let titleLabel: UILabel = {
         let titleLabel = UILabel()
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.font = .preferredFont(forTextStyle: .title1)
         titleLabel.adjustsFontForContentSizeCategory = true
-        titleLabel.textColor = .black
+        titleLabel.font = .preferredFont(forTextStyle: .title1)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         return titleLabel
     }()
     let lastModifiedDateLabel: UILabel = {
         let lastModifiedDateLabel = UILabel()
-        lastModifiedDateLabel.translatesAutoresizingMaskIntoConstraints = false
-        lastModifiedDateLabel.font = .preferredFont(forTextStyle: .body)
         lastModifiedDateLabel.adjustsFontForContentSizeCategory = true
-        lastModifiedDateLabel.textColor = .black
-        lastModifiedDateLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
-        lastModifiedDateLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        lastModifiedDateLabel.font = .preferredFont(forTextStyle: .body)
+        lastModifiedDateLabel.translatesAutoresizingMaskIntoConstraints = false
         return lastModifiedDateLabel
     }()
     let detailLabel: UILabel = {
         let detailLabel = UILabel()
-        detailLabel.translatesAutoresizingMaskIntoConstraints = false
-        detailLabel.font = .preferredFont(forTextStyle: .body)
-        detailLabel.adjustsFontForContentSizeCategory = true
         detailLabel.textColor = .gray
-        detailLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        detailLabel.adjustsFontForContentSizeCategory = true
+        detailLabel.font = .preferredFont(forTextStyle: .body)
+        detailLabel.translatesAutoresizingMaskIntoConstraints = false
         detailLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         return detailLabel
     }()
@@ -43,10 +40,6 @@ class NoteTableViewCell: UITableViewCell {
         setUpConstraints()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     private func setUpConstraints() {
         contentView.addSubview(titleLabel)
         contentView.addSubview(lastModifiedDateLabel)
@@ -54,17 +47,16 @@ class NoteTableViewCell: UITableViewCell {
         
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
-            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
             titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
             
             lastModifiedDateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
-            lastModifiedDateLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
             lastModifiedDateLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            lastModifiedDateLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
             
             detailLabel.leadingAnchor.constraint(equalTo: lastModifiedDateLabel.trailingAnchor, constant: 40),
             detailLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
-            detailLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
-            detailLabel.bottomAnchor.constraint(equalTo: lastModifiedDateLabel.bottomAnchor)
+            detailLabel.centerYAnchor.constraint(equalTo: lastModifiedDateLabel.centerYAnchor)
         ])
     }
     
@@ -72,5 +64,9 @@ class NoteTableViewCell: UITableViewCell {
         titleLabel.text = nil
         lastModifiedDateLabel.text = nil
         detailLabel.text = nil
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
