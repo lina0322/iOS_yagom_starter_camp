afe319a1138e2dbf9673ebc331a89bebb6b37cc0
taetae <taetae.dev@gmail.com>
2021-02-23 15:31:11 +0900

    refactor: UI 변경에 따른 버튼 추가 및 기능 구현

--- CloudNotes/CloudNotes/Controller/DetailViewController.swift
+++ CloudNotes/CloudNotes/Controller/DetailViewController.swift
@@ -24,6 +24,7 @@ final class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTextView()
+        configureNavigationItem()
     }
     
     private func configureTextView() {
@@ -40,6 +41,46 @@ final class DetailViewController: UIViewController {
         let content = NSMutableAttributedString(string: "\(noteTitle) \n\n", attributes: [.font: UIFont.preferredFont(forTextStyle: .title1)])
         content.append(NSMutableAttributedString(string: noteBody, attributes: [.font: UIFont.preferredFont(forTextStyle: .body)]))
         detailTextView.attributedText = content
+    }
+    
+    private func configureNavigationItem() {
+        let moreDetailButton: UIButton = UIButton()
+        moreDetailButton.setImage(UIImage(systemName: "ellipsis.circle"), for: .normal)
+        moreDetailButton.addTarget(self, action: #selector(touchUpMoreDetailButton), for: .touchUpInside)
+        let barButton =  UIBarButtonItem(customView: moreDetailButton)
+        navigationItem.rightBarButtonItem = barButton
+    }
+    
+    @objc private func touchUpMoreDetailButton() {
+        showActionSheet()
+    }
+    
+    private func showActionSheet() {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let shareButton = UIAlertAction(title: "Share...", style: .default, handler: { _ in self.showActivityView()})
+        let deleteButton = UIAlertAction(title: "Delete", style: .destructive, handler: { _ in self.showDeleteAlert()})
+        let cancleButton = UIAlertAction(title: "Cancle", style: .cancel, handler: nil)
+        
+        actionSheet.addAction(shareButton)
+        actionSheet.addAction(deleteButton)
+        actionSheet.addAction(cancleButton)
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    private func showActivityView() {
+        let shareItem = [noteTitle + "\n" + noteBody]
+        let activityViewController = UIActivityViewController(activityItems: shareItem, applicationActivities: nil)
+        present(activityViewController, animated: true, completion: nil)
+    }
+    
+    private func showDeleteAlert() {
+        let deleteAlert = UIAlertController(title: "진짜루?", message: "정말로 삭제하시겠어요?", preferredStyle: .alert)
+        self.present(deleteAlert, animated: true, completion: nil)
+        let deleteButton = UIAlertAction(title: "삭제", style: .destructive, handler: nil)
+        let cancleButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        deleteAlert.addAction(deleteButton)
+        deleteAlert.addAction(cancleButton)
     }
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
--- CloudNotes/CloudNotes/Controller/NoteTableViewController.swift
+++ CloudNotes/CloudNotes/Controller/NoteTableViewController.swift
@@ -38,39 +38,6 @@ final class NoteTableViewController: UITableViewController {
     
     @objc private func touchUpAddButton() {
         print("button pressed")
-        showActionSheet()
-    }
-    
-    private func showActionSheet() {
-        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        let shareButton = UIAlertAction(title: "Share...", style: .default, handler: { _ in self.showActivityView()})
-        let deleteButton = UIAlertAction(title: "Delete", style: .destructive, handler: { _ in self.showDeleteAlert()})
-        let cancleButton = UIAlertAction(title: "Cancle", style: .cancel, handler: nil)
-        
-        actionSheet.addAction(shareButton)
-        actionSheet.addAction(deleteButton)
-        actionSheet.addAction(cancleButton)
-        self.present(actionSheet, animated: true, completion: nil)
-    }
-    
-    private func showActivityView() {
-        guard let noteIndex = noteIndex else {
-            print("선택된 노트가 없음")
-            return
-        }
-        let shareItem = [noteList[noteIndex].title + "\n" + noteList[noteIndex].body]
-        let activityViewController = UIActivityViewController(activityItems: shareItem, applicationActivities: nil)
-        present(activityViewController, animated: true, completion: nil)
-    }
-    
-    private func showDeleteAlert() {
-        let deleteAlert = UIAlertController(title: "진짜루?", message: "정말로 삭제하시겠어요?", preferredStyle: .alert)
-        self.present(deleteAlert, animated: true, completion: nil)
-        let deleteButton = UIAlertAction(title: "삭제", style: .destructive, handler: nil)
-        let cancleButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
-        
-        deleteAlert.addAction(deleteButton)
-        deleteAlert.addAction(cancleButton)
     }
 }
 
