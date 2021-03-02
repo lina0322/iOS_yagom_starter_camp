a5727ce2bd415899df601d4ca4521e31b4cdcb32
lina0322 <llim@student.42seoul.kr>
2021-03-02 23:29:37 +0900

    fix: m1에서 pod install 오류 해결

--- CloudNotes/CloudNotes/Supporting Files/AppDelegate.swift
+++ CloudNotes/CloudNotes/Supporting Files/AppDelegate.swift
@@ -12,7 +12,7 @@ import SwiftyDropbox
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        DropboxClientsManager.setupWithAppKey("hsm53pga01pohr2") // Dropbox
+        DropboxClientsManager.setupWithAppKey("lxknrymie215lcv") // Dropbox
         return true
     }
     
